21d2dc60047f9f7dbaf7e37cd9e943c060282241
felipealvx <felipealvk@gmail.com>
2025-02-07 23:44:36 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import { createNote, notes } from './notes.js'
+import {v4 as uuidv4 } from 'uuid';
+
+const port = 3000;
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send("Bloco de cadastro de usuários");
+})
+
+app.get('/notes', (req, res) => {
+  return res.json(notes);
+})
+
+app.post('/notes', (req, res) => {
+  const { note } = req.body;
+
+  if (!note){
+    return res.status(400).json({message: 'Require!!'});
+  }
+
+  if (notes.some((notation) => notation.note === note)) {
+    return res.json({message: "Você ainda não cumpriu essa nota!"})
+  }
+
+  const newNote = {
+    id: uuidv4(),
+    note,
+  }
+  createNote(newNote);
+
+  return res.status(201).json({message: 'Note Create With Sucess!!'});
+})
+
+app.listen(port, () => {
+  console.log(`O servidor está sendo executado na porta: ${port}`)
+})
---
+++ src/notes.js
@@ -0,0 +1,10 @@
+export const notes = [
+  {
+    id: "1",
+    note: "Beber agua"
+  }
+]
+
+export const createNote = (note) => {
+  notes.push(note)
+};
